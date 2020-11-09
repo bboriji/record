dddac4cb0cd84117e184379b876122ff98917383
육진혁 <jdd04026@kakao.com>
2020-11-10 03:29:52 +0900

    Controller 계층을 추가함

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const router = require("./controllers");
+const { postRouter, userRouter } = require("./controllers");
+
+require('./database')();
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use('/api', postRouter);
+app.use('/api', userRouter)
 
 module.exports = app;

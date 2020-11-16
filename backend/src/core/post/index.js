52ef7f8ea851100aaa79705277bfba4a5e90e67f
육진혁 <jdd04026@kakao.com>
2020-11-17 02:39:45 +0900

    유저의 포스트를 불러오는 API 를 작성함

--- backend/src/core/post/index.js
+++ backend/src/core/post/index.js
@@ -5,25 +5,20 @@ const getPosts = async () => {
 }
 
 const getPost = async (id) => {
-  const post = await Post.findAll({
+  return await Post.findOne({
     where: { id }
   })
-  // 만약 포스트가 없으면 null 을 리턴한다
-  if(post.length === 0) {
-    return null
-  }
-  return post[0]
 }
 
-const getUserPosts = async (postId, userId) => {
+const getUserPostsByUserId = async (userId) => {
   const post = await Post.findAll({
-    where: { id: postId, userId }
+    where: { userId }
   })
   // 만약 포스트가 없으면 null 을 리턴한다
   if(post.length === 0) {
     return null
   }
-  return post[0]
+  return post
 }
 
-module.exports = { getPosts, getPost }
+module.exports = { getPosts, getPost, getUserPostsByUserId }

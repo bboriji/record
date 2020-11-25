6c890fc980623df7927193789128bda60f1a7381
육진혁 <jdd04026@kakao.com>
2020-11-25 18:17:55 +0900

    Relation 을 수정함

--- backend/src/core/post/index.js
+++ backend/src/core/post/index.js
@@ -7,14 +7,14 @@ const getPosts = async () => {
 const getPost = async (id) => {
   return await Post.findOne({
     where: { id },
-    include: [{ as: 'user', model: User,  exclude: ['passwd']  }],
+    include: [{ as: 'user', model: User }],
   })
 }
 
 const getUserPostsByUserId = async (userid) => {
   const post = await Post.findAll({
     where: { userid },
-    include: [{ as: 'user', model: User,  exclude: ['passwd']  }],
+    include: [{ as: 'user', model: User }],
   })
   // 만약 포스트가 없으면 null 을 리턴한다
   if (post.length === 0) {
